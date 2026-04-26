2a4c8110234bd89a5f68adb21c0057aa956255af
Jelena Tanaskovic <jelenatanaskovic@gmail.com>
2026-04-26 14:03:11 +0200

    l

--- api/reject-user.js
+++ api/reject-user.js
@@ -16,6 +16,13 @@ function getAdminApp() {
 }
 
 export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+if (req.method === "OPTIONS") {
+  return res.status(200).end();
+}
   const { uid, type } = req.query;
 
   if (!uid || !type) {
